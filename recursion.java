f4db1dd44b68d72afef61eb6f9a5959958268fc2
Vuottek Un <vuottek.un@westpoint.edu>
2023-11-23 13:26:41 +0700

    Completed

--- recursion.java
+++ recursion.java
@@ -1,15 +1,15 @@
-public class recursion {
-  public static void main(String[] args) {
-
-    //call your recursion fuction
-    //print result
-
-   }
-    
-    public double.... recursionFunction {
-      //implement your case algorithm
-      return....
+public class Recursion {
+    public static void main(String[] args) {
+        int number = 20; // Example number
+        double result = factorial(number);
+        System.out.println("Factorial of " + number + " is: " + result);
     }
-  
+
+    public static double factorial(int n) {
+        if (n == 0) {  // Base case
+            return 1;
+        } else {
+            return n * factorial(n - 1); // Recursive call
+        }
+    }
 }
-    
